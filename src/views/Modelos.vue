49ac755a5dcabc23c5743821ead7ba5bf90ccb4c
Duarte Thiago <duartetbatista@gmail.com>
2022-09-22 08:53:28 -0300

    fix img

--- src/views/Modelos.vue
+++ src/views/Modelos.vue
@@ -10,7 +10,7 @@
           Cada linha, também chamada de tupla, inclui dados sobre uma instância específica da entidade em questão, como um determinado colaborador.
           O modelo foi introduzido por E. F. Codd em 1970.
       </p>
-      <img class="img" src="src\views\img\modelo_relacional.jpg" alt="" height="500">
+      <img class="img" src="src\views\img\modelo_relacional.jpg" alt="modelo relacional" height="500">
     </div>
     <div class="container">
       <h3>
@@ -20,7 +20,7 @@
           O modelo hierárquico organiza dados em uma estrutura do tipo árvore, onde cada registro tem um único "pai" ou raiz. Registros "irmãos" são classificados em uma ordem específica. Essa ordem é usada como a ordem física para armazenar o banco de dados. Este modelo é bom para descrever muitas relações do mundo real.
           Esse modelo foi usado principalmente pelos Sistemas de Gestão de Informações da IBM nos anos 60 e 70, mas são raramente vistos hoje devido a certas ineficiências operacionais.   
       </p>
-      <img class="img" src="src\views\img\modelo_hierarquico.jpg" alt="" height="300">
+      <img class="img" src="src\views\img\modelo_hierarquico.jpg" alt="modelo hierarquico" height="300">
     </div>
     <div class="container">
       <h3>
@@ -30,7 +30,7 @@
           O modelo de rede se baseia no modelo hierárquico, permitindo relações muitas para muitas entre registros vinculados, implicando em vários registros "pai". Baseado na teoria de conjuntos matemáticos, o modelo é construído com conjuntos de registros relacionados. Cada conjunto consiste em um registro proprietário, ou "pai", e um ou mais registros de membro, ou "filho". Um registro pode ser um membro, ou "filho", em vários conjuntos, permitindo que esse modelo transmita relações complexas.
           Foi mais popular nos anos 70, depois de ter sido formalmente definido pela Conferência sobre Linguagens de Sistemas de Dados (CODASYL).    
       </p>
-      <img class="img" src="src\views\img\modelo_rede.jpg" alt="" height="400">
+      <img class="img" src="src\views\img\modelo_rede.jpg" alt="modelo rede" height="400">
     </div>
     <div class="container">
       <h3>
@@ -42,7 +42,7 @@
           Um banco de dados de hipertexto permite que qualquer objeto seja vinculado a qualquer outro objeto. É útil para organizar lotes de dados diferentes, mas não é ideal para a análise numérica.
           O modelo de banco de dados orientado a objetos é o modelo de banco de dados pós-relacional mais conhecido, uma vez que ele incorpora tabelas, mas não se limita a elas. Tais modelos também são conhecidos como modelos de bancos de dados híbridos.
       </p>
-      <img class="img" src="src\views\img\modelo_orientado_objetos.jpg" alt="" height="400">
+      <img class="img" src="src\views\img\modelo_orientado_objetos.jpg" alt="modelo orientado objetos" height="400">
     </div>
   </div>
 </template>
